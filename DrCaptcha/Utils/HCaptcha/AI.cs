3b2831daaf55391f10933e425592c9687fd07c68
kayle <kayleigh2310@outlook.com>
2023-08-11 12:51:13 +0100

    Add project files.

---
+++ DrCaptcha/Utils/HCaptcha/AI.cs
@@ -0,0 +1,84 @@
+﻿using Clarifai.Api;
+using Clarifai.Channels;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DrCaptcha.Utils.HCaptcha
+{
+    internal class AI
+    {
+        static bool FindMatchingSubword(string keyword, string predictedConcepts)
+        {
+            string[] conceptsArray = predictedConcepts.Split('\n');
+
+            if (Array.Exists(conceptsArray, concept => concept.Contains(keyword)))
+            {
+                return true;
+            }
+
+            foreach (string predictedWord in conceptsArray)
+            {
+                for (int i = 0; i < predictedWord.Length; i++)
+                {
+                    for (int j = i + 4; j <= predictedWord.Length; j++)
+                    {
+                        string subword = predictedWord.Substring(i, j - i);
+                        if (subword.Length > 4 && keyword.Contains(subword))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static string[] Recognise(dynamic task, string keyword)
+        {
+            V2.V2Client client = new V2.V2Client(ClarifaiChannel.Grpc());
+            Metadata metadata = new Metadata
+             {
+                {"Authorization", "Key 48e4bb33c47747378d4c2f9a115aa06f"}
+            };
+            string imagelink = task["datapoint_uri"].ToString();
+            string taskkey = task["task_key"].ToString();
+            var response = client.PostModelOutputs(
+                new PostModelOutputsRequest()
+                {
+                    ModelId = "aaa03c23b3724a16a56b629203edc62c", // <- This is the general model_id
+                    Inputs =
+                    {
+                        new List<Input>()
+                        {
+                            new Input()
+                            {
+                                Data = new Data()
+                                {
+                                    Image = new Clarifai.Api.Image()
+                                    {
+                                        Url = imagelink
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                metadata
+            );
+            foreach (var concept in response.Outputs[0].Data.Concepts)
+            {
+                bool result = FindMatchingSubword(keyword, concept.Name);
+                if (result)
+                {
+                    return new string[] { taskkey, "true" };
+                }
+            }
+            return new string[] { taskkey, "false" };
+        }
+    }
+}
---
+++ DrCaptcha/Utils/HCaptcha/API.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DrCaptcha.Utils.HCaptcha
+{
+    internal class API
+    {
+        public static async Task<dynamic> CheckSiteKey(HttpClient Client, string version, string host, string sitekey)
+        {
+            HttpResponseMessage response = await Client.PostAsync($"https://api2.hcaptcha.com/checksiteconfig?v={version}&host={host}&sitekey={sitekey}&sc=1&swa=1&spst=0", null);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public static async Task<string> GetHsw(HttpClient Client, string req)
+        {
+            string payload = JsonConvert.SerializeObject(new
+            {
+                script = "https://newassets.hcaptcha.com/c/278beb8b/hsw.js",
+                req = req
+            });
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await Client.PostAsync(new Uri("https://hcaptcha.vxxx.cf/hsw"), content);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result)["result"];
+        }
+
+        public static async Task<string> GetVersion(HttpClient Client)
+        {
+            HttpResponseMessage response = await Client.GetAsync("https://hcaptcha.com/1/api.js");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                string pattern = @"https:\/\/newassets.hcaptcha.com\/captcha\/v1\/(\w+)\/static";
+                Match match = Regex.Match(data, pattern);
+                if (match.Success && match.Groups.Count > 1)
+                {
+                    return match.Groups[1].Value;
+                }
+            }
+            return "";
+        }
+
+        public static async Task<dynamic> GetCaptcha(HttpClient Client, string version, string host, string sitekey, string c, string n, string motiondata)
+        {
+
+            var regdata = new[]
+            {
+                new KeyValuePair<string, string>("v",version),
+                new KeyValuePair<string, string>("sitekey", sitekey),
+                new KeyValuePair<string, string>("host", host),
+                new KeyValuePair<string, string>("hl", "en"),
+                new KeyValuePair<string, string>("c", c),
+                new KeyValuePair<string, string>("n", n),
+                new KeyValuePair<string, string>("motionData", motiondata)
+            };
+
+            HttpResponseMessage response = await Client.PostAsync($"https://hcaptcha.com/getcaptcha/{sitekey}", new FormUrlEncodedContent(regdata));
+            return JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+        }
+
+        public static async Task<dynamic> SubmitCaptcha(HttpClient Client, string version, string host, string sitekey, string key, string c, string n, string motiondata, Dictionary<string, string> answers)
+        {
+            string link = $"https://hcaptcha.com/checkcaptcha/{sitekey}/{key}";
+            string payload = JsonConvert.SerializeObject(new
+            {
+                answers = answers,
+                c = c,
+                job_mode = "image_label_binary",
+                n = n,
+                serverdomain = host,
+                motionData = motiondata,
+                sitekey = sitekey,
+                v = version
+            });
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await Client.PostAsync(link, content);
+            return JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
